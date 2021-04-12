fe177d14db0b1e1a5ba8999c142af5161d7461de
mirwox <fmiranda@gmail.com>
2021-04-12 15:10:48 -0300

    gabarito da questao do gato

--- sim211/scripts/Q2.py
+++ sim211/scripts/Q2.py
@@ -56,14 +56,23 @@ tfl = 0
 
 tf_buffer = tf2_ros.Buffer()
 
+dists = None
+
+min_laser = 1000
+max_laser = 0
 
 def scaneou(dado):
-	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
-	print("Leituras:")
-	print(np.array(dado.ranges).round(decimals=2))
-	#print("Intensities")
-	#print(np.array(dado.intensities).round(decimals=2))
-
+    print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+    print("Leituras:")
+    global dists
+    dists = np.array(dado.ranges).round(decimals=2)
+    print(dists)
+    global min_laser
+    global max_laser 
+    min_laser = dado.range_min 
+    max_laser = dado.range_max
+    #print("Intensities")
+    #print(np.array(dado.intensities).round(decimals=2))
 
 
 # A função a seguir é chamada sempre que chega um novo frame
@@ -98,7 +107,21 @@ def roda_todo_frame(imagem):
         cv2.waitKey(1)
     except CvBridgeError as e:
         print('ex', e)
+
+
+def valido(leitura):
+    return min_laser <= leitura <= max_laser
+
+
+def esta_proximo(laser):
+    if valido(laser[5]) and valido(laser[355]):
+        media = (laser[355] + laser[5])/2
+        if media <= 0.8:
+            return True
+    return False
     
+
+
 if __name__=="__main__":
     rospy.init_node("Q2")
 
@@ -113,6 +136,32 @@ if __name__=="__main__":
 
     tolerancia = 25
 
+    AVANCAR = 1
+    CENTRALIZAR = 2
+    FINAL = 3
+
+    estado = AVANCAR 
+
+
+    v = 0.2
+    w = 0.3
+
+    c = 320 # centro da tela - SEMPRE CHECAr RESOLUCAO
+
+
+    # 
+    zero = Twist(Vector3(0,0,0), Vector3(0,0,0.0))
+    # 
+    frente = Twist(Vector3(v,0,0), Vector3(0,0,0.0))
+    # 
+    direita = Twist(Vector3(0,0,0), Vector3(0,0, -w ))
+
+    esquerda = Twist(Vector3(0,0,0), Vector3(0,0,w))
+
+    proximo = False
+
+    mg_centro = 10 # tolerancia de centralizar
+
     # Exemplo de categoria de resultados
     # [('chair', 86.965459585189819, (90, 141), (177, 265))]
 
@@ -121,10 +170,49 @@ if __name__=="__main__":
         # vel = Twist(Vector3(0,0,0), Vector3(0,0,math.pi/10.0))
         
         while not rospy.is_shutdown():
-            for r in resultados:
-                print(r)
-            #velocidade_saida.publish(vel)
 
+
+            ### Gabarito: Avancar em frente ate ficar a 0.8m da parede 
+            ## Risco - creeper atrapalhar 
+            if estado == AVANCAR: 
+                # avancar
+                velocidade_saida.publish(frente)
+
+                # checar se os lasers detectam parede
+                # a 0.8 ou mais proximo
+                if dists is not None:  
+                    proximo = esta_proximo(dists)                    
+                if proximo: 
+                    estado = CENTRALIZAR
+                    velocidade_saida.publish(zero)
+                    rospy.sleep(0.5)
+                    velocidade_saida.publish(esquerda)
+
+
+            if estado == CENTRALIZAR:
+                ### Gabarito: Girar ate ver o gato centralizado
+                ### Risco: gato nao detectado
+                for r in resultados:
+                    mc = mg_centro
+                    mx = 100
+                    
+                    if r[0] == "cat":
+                        mx = (r[2][0] + r[3][0])/ 2
+                        mx = int(mx)
+                    
+                    if c - mc < mx < c + mc:
+                        estado = FINAL
+                        velocidade_saida.publish(zero)
+                    elif mx < c - mc:
+                        # virar esq 
+                        velocidade_saida.publish(esquerda)
+                    elif  mx > c + mc:
+                        # dirar direita  
+                        velocidade_saida.publish(direita)
+            if estado == FINAL:
+                velocidade_saida.publish(zero)
+                rospy.sleep(4.0)
+                break
             rospy.sleep(0.1)
 
     except rospy.ROSInterruptException:
